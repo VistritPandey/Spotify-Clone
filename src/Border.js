8f8cf6cb2e7a66034dc40b81214f4582469149c8
vistritpandey <vistritpandey@gmail.com>
2020-11-05 18:29:08 +0530

    Sidebar updated

---
+++ src/Border.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Border.css'
+
+function Border() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Border
---
+++ src/SidebarOption.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarOption() {
+    return (
+        <div>
+            <h1>I am the option</h1>
+        </div>
+    )
+}
+
+export default SidebarOption
