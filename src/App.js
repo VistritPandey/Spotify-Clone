db4b7debf5c03e59d02819e627063996f829bc9a
vistritpandey <vistritpandey@gmail.com>
2020-11-05 17:53:38 +0530

    okay

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
+import SpotifyWebApi from "spotify-web-api-js";
 import { getTokenFromResponse } from './spotify';
 
+const s = new SpotifyWebApi();
+
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ token }, dispatch] = useState();
 
-  useEffect( () => {
+  useEffect(() => {
+    
     const hash = getTokenFromResponse();
-
     window.location.hash = "";
     let _token = hash.access_token;
+
     if (_token) {
-      setToken(_token);
+      s.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
     }
-  }, [])
+  }, [token, dispatch]);
+
   return (
     <div className="App">
       {!token && <Login />}
