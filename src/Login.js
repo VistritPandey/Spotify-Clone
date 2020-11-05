554e9331e566e0fee32e8e7248fe1c760f9e2b9a
vistritpandey <vistritpandey@gmail.com>
2020-11-05 09:27:20 +0530

    Login Setup complete

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Login.css'
+import { accessUrl } from "./spotify"
 
 function Login() {
     return (
@@ -8,7 +9,7 @@ function Login() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt=""
             />
-            <a>Login with Spotify</a>
+            <a href={accessUrl}>Login with Spotify</a>
         </div>
     )
 }
