5a9d170222c4e84970fab137e2b2bb2855ed21ec
vistritpandey <vistritpandey@gmail.com>
2020-11-05 09:14:37 +0530

    okay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Login from './Login'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className='login'>
+            <h3>This is the login page</h3>
+        </div>
+    )
+}
+
+export default Login
