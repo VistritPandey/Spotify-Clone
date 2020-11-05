25aecb170357ca8bc044db58e02b4fbb9c70b939
vistritpandey <vistritpandey@gmail.com>
2020-11-05 18:52:46 +0530

    Song Row complete

--- src/SongRow.js
+++ src/SongRow.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-function SongRow() {
-    return (
-        <div className='songrow'>
-            <h1> Song Row</h1>
-        </div>
-    )
+import "./SongRow.css";
+
+function SongRow({ track, playSong }) {
+
+  return (
+    <div className="songRow" onClick={() => playSong(track.id)}>
+      <img className="songRow__album" src={track.album.images[0].url} alt="" />
+      <div className="songRow__info">
+        <h1>{track.name}</h1>
+        <p>
+          {track.artists.map((artist) => artist.name).join(", ")} -{" "}
+          {track.album.name}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default SongRow
+export default SongRow;
