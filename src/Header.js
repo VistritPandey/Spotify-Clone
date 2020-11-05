27d4adb41deda865f50b4d00a2dd008f997e0529
vistritpandey <vistritpandey@gmail.com>
2020-11-05 18:55:19 +0530

    Fixed Header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import "./Header.css";
+import { useStateValue } from "./StateProvider";
+import { Avatar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
-function Header() {
-    return (
-        <div className='header'>
-            <h1>I am the header</h1>
-        </div>
-    )
+function Header({ spotify }) {
+  const [{ user }, dispatch] = useStateValue();
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input
+          placeholder="Search for Artists, Songs, or Podcasts "
+          type="text"
+        />
+      </div>
+      <div className="header__right">
+        <Avatar alt={user?.display_name} src={user?.images[0].url} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Header 
