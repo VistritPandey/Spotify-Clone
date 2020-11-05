435dfda17859a8892e92985c857a2733219674e6
vistritpandey <vistritpandey@gmail.com>
2020-11-05 09:26:09 +0530

    Jio Done Dona Done

---
+++ src/spotify.js
@@ -0,0 +1,26 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "4b13547d444649178e8211f401811a7e";
+
+const redirectUri = "http://localhost:3000/";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
