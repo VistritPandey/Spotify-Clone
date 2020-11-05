b186975d71f6c3913301056b83b07a6e1433780e
vistritpandey <vistritpandey@gmail.com>
2020-11-05 18:50:41 +0530

    Okay

---
+++ src/SongRow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SongRow() {
+    return (
+        <div className='songrow'>
+            <h1> Song Row</h1>
+        </div>
+    )
+}
+
+export default SongRow
