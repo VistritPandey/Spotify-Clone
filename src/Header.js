ecb3b2bc0635a8d2ce6b10cfb6d9071aa37b112e
vistritpandey <vistritpandey@gmail.com>
2020-11-05 18:48:51 +0530

    Header OP in the chat

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>I am the header</h1>
+        </div>
+    )
+}
+
+export default Header 
