c0136e05f07780f89aae255ae5066c42c3b8ba5f
vistritpandey <vistritpandey@gmail.com>
2020-11-05 09:41:58 +0530

    Okay......

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
 import Login from './Login'
+import { getTokenFromResponse } from './spotify';
 
 function App() {
+
+  useEffect( () => {
+    const token = getTokenFromResponse()
+    console.log('I have a noob', token)
+  }, [])
+
   return (
     <div className="App">
       <Login />
